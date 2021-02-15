5cee8169d79d6818de4f6b2108f2d9bb08f39c49
husseinraed <me@husseinraed.cf>
2021-02-15 20:28:39 +0300

    changed version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ini.py",
-    version="0.0.3",
+    version="0.0.4",
     author="Hussein Raed",
     author_email="me@husseinraed.cf",
     description="a lightweight ini parser",
